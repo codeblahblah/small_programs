861c00a7ce777aac56a09cadc29d2829b1d13936
drammopo <drammopo@gmail.com>
2016-03-27 21:37:36 +0200

    Removed optional parentheses when calling methods as well as Kernel.

--- lesson_2/calculator/README.md
+++ lesson_2/calculator/README.md
@@ -2,3 +2,4 @@
 ## Things to think about:
 1. Is there a better way to validate that the user has input a number?
 2. What if we needed to add some code after the case statement within the `operation_to_message` method?
+3. There are lots of messages sprinkled throughout the program. Could we move them into some configuration file and access by key? This would allow us to manage the messages much easier, and we could even internationalize the messages. This is just a thought experiment, and no need to code this up.
--- lesson_2/calculator/calculator.rb
+++ lesson_2/calculator/calculator.rb
@@ -1,9 +1,9 @@
 def prompt(message)
-  Kernel.puts("=> #{message}")
+  puts("=> #{message}")
 end
 
 def valid_number?(num)
-  num.to_i() != 0
+  num.to_i != 0
 end
 
 def operation_to_message(op)
@@ -23,9 +23,9 @@ prompt("Welcome to Calculator! Enter your name:")
 
 name = ''
 loop do
-  name = Kernel.gets().chomp()
+  name = gets.chomp
 
-  if name.empty?()
+  if name.empty?
     prompt("Make sure to use a valid name.")
   else
     break
@@ -38,7 +38,7 @@ loop do # main loop
   number1 = ''
   loop do
     prompt("What's the first number?")
-    number1 = Kernel.gets().chomp()
+    number1 = gets.chomp
 
     if valid_number?(number1)
       break
@@ -50,7 +50,7 @@ loop do # main loop
   number2 = ''
   loop do
     prompt("What's the second number?")
-    number2 = Kernel.gets().chomp()
+    number2 = gets.chomp
 
     if valid_number?(number2)
       break
@@ -71,7 +71,7 @@ loop do # main loop
 
   operator = ''
   loop do
-    operator = Kernel.gets().chomp()
+    operator = gets.chomp
 
     if %w(1 2 3 4).include?(operator)
       break
@@ -84,20 +84,20 @@ loop do # main loop
 
   result = case operator
            when '1'
-             number1.to_i() + number2.to_i()
+             number1.to_i + number2.to_i
            when '2'
-            number1.to_i() - number2.to_i()
+            number1.to_i - number2.to_i
            when '3'
-            number1.to_i() * number2.to_i()
+            number1.to_i * number2.to_i
            when '4'
-            number1.to_f() / number2.to_f()
+            number1.to_f / number2.to_f
   end
 
   prompt("The result is #{result}")
 
   prompt("Do you want to perform another calculation? (Y to calculate again)")
-  answer = Kernel.gets().chomp()
-  break unless answer.downcase().start_with?('y')
+  answer = gets.chomp
+  break unless answer.downcase.start_with?('y')
 end
 
 prompt("Thank you for using the calculator. Good bye!")
