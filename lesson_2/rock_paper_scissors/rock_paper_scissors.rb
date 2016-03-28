cec44a67be52ad633ac854cf43cec13fd27668e0
drammopo <drammopo@gmail.com>
2016-03-28 23:13:29 +0200

    RPS walk-through.

---
+++ lesson_2/rock_paper_scissors/rock_paper_scissors.rb
@@ -0,0 +1,42 @@
+VALID_CHOICES = %w(rock paper scissors).freeze
+
+def prompt(message)
+  puts("=> #{message}")
+end
+
+def display_output(player, computer)
+  if (player == 'rock' && computer == 'scissors') ||
+     (player == 'paper' && computer == 'rock') ||
+     (player == 'scissors' && computer == 'paper')
+    prompt("You won!")
+  elsif (player == 'rock' && computer == 'paper') ||
+        (player == 'paper' && computer == 'scissors') ||
+        (player == 'scissors' && computer == 'rock')
+    prompt("Computer won!")
+  else
+    prompt("It's a tie!")
+  end
+end
+
+loop do
+  choice = ''
+  loop do
+    prompt("Choose one: #{VALID_CHOICES.join(', ')}")
+    choice = gets.chomp
+
+    break if VALID_CHOICES.include?(choice)
+    prompt("That's not a valid choice.")
+  end
+
+  computer_choice = VALID_CHOICES.sample
+
+  prompt("You choose: #{choice}; Computer chose: #{computer_choice}")
+
+  display_output(choice, computer_choice)
+
+  prompt("Do you want to play again?")
+  answer = gets.chomp
+  break unless answer.downcase.start_with?('y')
+end
+
+prompt("Thank you for playing. Good bye!")
