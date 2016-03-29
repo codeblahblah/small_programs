83965e25a1beeb280f2f46855e75a76f86e04815
drammopo <drammopo@gmail.com>
2016-03-29 16:27:40 +0200

    RPS Bonus Features

--- lesson_2/rock_paper_scissors/rock_paper_scissors.rb
+++ lesson_2/rock_paper_scissors/rock_paper_scissors.rb
@@ -1,4 +1,10 @@
-VALID_CHOICES = %w(rock paper scissors spock lizard).freeze
+VALID_CHOICES = {
+                  r: 'rock',
+                  p: 'paper',
+                  s: 'scissors',
+                  S: 'spock',
+                  l: 'lizard'
+                }.freeze
 
 def prompt(message)
   puts("=> #{message}")
@@ -14,37 +20,47 @@ def win?(first, second)
     (first == 'spock' && second == 'scissors') ||
     (first == 'spock' && second == 'rock') ||
     (first == 'lizard' && second == 'spock') ||
-    (first == 'lizard' && second == 'paper') ||
-    
-    (first == 'scissors' && second == 'lizard')
-    
+    (first == 'lizard' && second == 'paper')
 end
 
-def display_output(player, computer)
+def display_results(player, computer)
   if win?(player, computer)
-    prompt("You won!")
+    "You won!"
   elsif win?(computer, player)
-    prompt("Computer won!")
+    "Computer won!"
   else
-    prompt("It's a tie!")
+    "It's a tie!"
   end
 end
+
+wins = 0
+computer_wins = 0
 
 loop do
   choice = ''
   loop do
-    prompt("Choose one: #{VALID_CHOICES.join(', ')}")
-    choice = gets.chomp
+    prompt("Choose one: #{VALID_CHOICES.map { |k, v| "#{v} (#{k})" }.join(', ')}")
+    choice = VALID_CHOICES[gets.chomp.to_sym]
 
-    break if VALID_CHOICES.include?(choice)
+    break if VALID_CHOICES.values.include?(choice)
     prompt("That's not a valid choice.")
   end
 
-  computer_choice = VALID_CHOICES.sample
+  computer_choice = VALID_CHOICES.values.sample
 
-  prompt("You choose: #{choice}; Computer chose: #{computer_choice}")
+  prompt("You chose: #{choice}; Computer chose: #{computer_choice}")
 
-  display_output(choice, computer_choice)
+  wins += 1 if display_results(choice, computer_choice) == "You won!"
+  computer_wins += 1 if display_results(choice, computer_choice) == "Computer won!"
+
+  prompt(display_results(choice, computer_choice))
+
+  if wins == 5 || computer_wins == 5
+    prompt("We have a winner. Final score: #{wins}; Computer: #{computer_wins}")
+    break
+  else
+    prompt("The current score is: #{wins}; Computer: #{computer_wins}.")
+  end
 
   prompt("Do you want to play again?")
   answer = gets.chomp
