6b03c6bdf75732dd6e353996f3257ee5407ed179
drammopo <drammopo@gmail.com>
2016-04-08 21:53:06 +0200

    Twenty-One: Bonus Features

--- lesson_4/twenty_one/twenty_one.rb
+++ lesson_4/twenty_one/twenty_one.rb
@@ -1,5 +1,7 @@
 SUITS = [:hearts, :diamonds, :clubs, :spades].freeze
 RANKS = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 'Ace'].freeze
+GAME_LIMIT = 21
+DEALER_LIMIT = 17
 
 def prompt(msg)
   puts "=> #{msg}"
@@ -28,23 +30,23 @@ def score(hand)
   end
 
   values.select { |value| value == 'Ace' }.count.times do
-    sum -= 10 if sum > 21
+    sum -= 10 if sum > GAME_LIMIT
   end
 
   sum
 end
 
 def busted?(hand)
-  score(hand) > 21
+  score(hand) > GAME_LIMIT
 end
 
 def determine_winner(player_hand, dealer_hand)
   player_total = score(player_hand)
   dealer_total = score(dealer_hand)
 
-  if player_total > 21
+  if player_total > GAME_LIMIT
     :player_busted
-  elsif dealer_total > 21
+  elsif dealer_total > GAME_LIMIT
     :dealer_busted
   elsif dealer_total < player_total
     :player
@@ -72,12 +74,26 @@ def display_result(player_hand, dealer_hand)
   end
 end
 
+def display_result_for_round(player_hand, dealer_hand)
+  puts "=============="
+  prompt "Dealer has #{dealer_hand}, for a total of: #{score(dealer_hand)}"
+  prompt "Player has #{player_hand}, for a total of: #{score(player_hand)}"
+  puts "=============="
+end
+
 def play_again?
   puts "----------"
   prompt "Play again? (y or n)"
   answer = gets.chomp
   answer.downcase.start_with?('y')
 end
+
+def game_over?(player_wins, dealer_wins)
+  player_wins == 5 || dealer_wins == 5
+end
+
+player_wins = 0
+dealer_wins = 0
 
 loop do
   prompt "Welcome to Twenty-One!"
@@ -114,37 +130,43 @@ loop do
   end
 
   if busted?(player_hand)
+    display_result_for_round(player_hand, dealer_hand)
     display_result(player_hand, dealer_hand)
+    dealer_wins += 1
     play_again? ? next : break
   else
     puts "You chose to stay at #{score(player_hand)}"
+    display_result_for_round(player_hand, dealer_hand)
   end
 
   prompt "Dealer turn..."
 
   loop do
-    break if busted?(dealer_hand) || score(dealer_hand) >= 17
+    break if busted?(dealer_hand) || score(dealer_hand) >= DEALER_LIMIT
 
     prompt "Dealer hits!"
     dealer_hand << deal_card(deck)
     prompt "Dealer's cards are now: #{dealer_hand}"
   end
 
+  dealer_total ||= score(dealer_hand)
   if busted?(dealer_hand)
-    prompt "Dealer total is now: #{score(dealer_hand)}"
+    # prompt "Dealer total is now: #{dealer_total}"
+    display_result_for_round(player_hand, dealer_hand)
     display_result(player_hand, dealer_hand)
+    player_wins += 1
     play_again? ? next : break
   else
-    prompt "Dealer stays at #{score(dealer_hand)}"
+    prompt "Dealer stays at #{dealer_total}"
   end
 
-  puts "=============="
-  prompt "Dealer has #{dealer_hand}, for a total of: #{score(dealer_hand)}"
-  prompt "Player has #{player_hand}, for a total of: #{score(player_hand)}"
-  puts "=============="
-
+  display_result_for_round(player_hand, dealer_hand)
   display_result(player_hand, dealer_hand)
-  break unless play_again?
+
+  player_wins += 1 if determine_winner(player_hand, dealer_hand) == :player
+  dealer_wins += 1 if determine_winner(player_hand, dealer_hand) == :dealer
+
+  break unless play_again? # Exist the entire game
 end
 
 prompt "Thanks for playing Twenty One! Good bye!"
